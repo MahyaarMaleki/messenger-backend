e303e22f87b77fcdd9fbb381654d3cf5b349e719
Mahyar Maleki <mahyarmaleki2003@gmail.com>
2025-12-15 20:27:02 +0330

    Define user queries

---
+++ database/queries/users.sql
@@ -0,0 +1,49 @@
+-- name: CreateUser :one
+INSERT INTO users (
+    username,
+    email,
+    password_hash,
+    first_name,
+    last_name
+) VALUES (
+    $1, $2, $3, $4, $5
+) RETURNING *;
+
+-- name: GetUserByEmail :one
+SELECT * FROM users
+WHERE email = $1 LIMIT 1;
+
+-- name: GetUserByUsername :one
+SELECT * FROM users
+WHERE username = $1 LIMIT 1;
+
+-- name: GetUserById :one
+SELECT * FROM users
+WHERE id = $1 LIMIT 1;
+
+-- name: UpdateUser :one
+UPDATE users
+SET
+    first_name = COALESCE(sqlc.narg('first_name'), first_name),
+    last_name = COALESCE(sqlc.narg('last_name'), last_name),
+    bio = COALESCE(sqlc.narg('bio'), bio),
+    avatar_url = COALESCE(sqlc.narg('avatar_url'), avatar_url),
+    updated_at = now()
+WHERE id = sqlc.arg('id')
+RETURNING *;
+
+-- name: UpdatePassword :exec
+UPDATE users
+SET
+    password_hash = $2,
+    updated_at = now()
+WHERE id = $1;
+
+-- name: ListUsers :many
+SELECT * FROM users
+WHERE
+    username ILIKE '%' || $1 || '%'
+   OR first_name ILIKE '%' || $1 || '%'
+   OR last_name ILIKE '%' || $1 || '%'
+ORDER BY username
+LIMIT 50;
