c5bf33ef776d5c3b6b9c5014bfdac4780333e5bd
Mahyar Maleki <mahyarmaleki2003@gmail.com>
2025-12-15 17:15:32 +0330

    Define users and sessions schema

---
+++ db/migrations/20251210184823_init_users_sessions.sql
@@ -0,0 +1,41 @@
+-- +goose Up
+CREATE TABLE users (
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    username varchar NOT NULL UNIQUE,
+    email varchar NOT NULL UNIQUE,
+    password_hash varchar NOT NULL,
+
+    -- Profile Information
+    first_name varchar NOT NULL,
+    last_name varchar NOT NULL,
+    bio varchar,
+    birthday date,
+    avatar_url varchar,
+
+    created_at timestamptz NOT NULL DEFAULT (now()),
+    updated_at timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE sessions (
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id uuid NOT NULL,
+    refresh_token varchar NOT NULL,
+    user_agent varchar NOT NULL, -- To show "Logged in on Chrome/Windows"
+    client_ip varchar NOT NULL,  -- For security alerts
+    is_blocked boolean NOT NULL DEFAULT false, -- To revoke a specific device
+    expires_at timestamptz NOT NULL,
+    created_at timestamptz NOT NULL DEFAULT (now())
+);
+
+-- Foreign Key: If user is deleted, delete their sessions
+ALTER TABLE sessions ADD FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE;
+
+-- Indexes for performance
+CREATE INDEX ON users (username);
+CREATE INDEX ON users (email);
+CREATE INDEX ON sessions (user_id);
+
+-- +goose Down
+ALTER TABLE sessions DROP CONSTRAINT IF EXISTS sessions_user_id_fkey;
+DROP TABLE IF EXISTS sessions;
+DROP TABLE IF EXISTS users;
