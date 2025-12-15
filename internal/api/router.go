4cbb230131c5a33e24dc01d98199ffceb4964f95
Mahyar Maleki <mahyarmaleki2003@gmail.com>
2025-12-16 02:13:44 +0330

    Minor changes to test user creation route and also add a middleware

--- internal/api/router.go
+++ internal/api/router.go
@@ -1,7 +1,9 @@
 package api
 
 import (
+	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -10,21 +12,23 @@ import (
 func (server *Server) setupRouter() {
 	r := chi.NewRouter()
 
+	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
-	r.Use(middleware.RealIP)
+	r.Use(middleware.Timeout(60 * time.Second))
+	r.Use(middleware.SetHeader("Content-Type", "application/json"))
 
 	// Server Health Check
 	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("OK"))
+		_ = json.NewEncoder(w).Encode(map[string]string{"ping": "pong"})
 	})
 
 	// API Routes
 	r.Route("/api", func(r chi.Router) {
 		// User Routes
 		r.Route("/users", func(r chi.Router) {
-			// Routes here
+			r.Post("/", server.createUser)
 		})
 
 		// Session Routes
