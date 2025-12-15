94a5db68ed6462cfe3398a9c22e88ebcba60e33a
Mahyar Maleki <mahyarmaleki2003@gmail.com>
2025-12-15 20:42:18 +0330

    Load config and set up a new postgres connection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/mahyaarmaleki/messenger-backend/internal/config"
+	"github.com/mahyaarmaleki/messenger-backend/internal/db"
+)
 
 func main() {
-	fmt.Println("Starting out!")
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatal("cannot load config: ", err)
+	}
+
+	connPool, err := db.NewConnection(cfg.DBSource)
+	if err != nil {
+		log.Fatal("cannot connect to database: ", err)
+	}
+
+	defer connPool.Close()
+
+	store := db.NewStore(connPool)
+
+	// TODO: set up the server
 }
