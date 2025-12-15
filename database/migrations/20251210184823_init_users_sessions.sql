d06aa538463c4e25c837ebefac2c777d0949463c
Mahyar Maleki <mahyarmaleki2003@gmail.com>
2025-12-16 02:30:44 +0330

    Add some length constraints on text/string fields

--- database/migrations/20251210184823_init_users_sessions.sql
+++ database/migrations/20251210184823_init_users_sessions.sql
@@ -1,14 +1,14 @@
 -- +goose Up
 CREATE TABLE users (
     id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
-    username varchar NOT NULL UNIQUE,
-    email varchar NOT NULL UNIQUE,
+    username varchar(30) NOT NULL UNIQUE,
+    email varchar(255) NOT NULL UNIQUE,
     password_hash varchar NOT NULL,
 
     -- Profile Information
-    first_name varchar NOT NULL,
-    last_name varchar NOT NULL,
-    bio varchar,
+    first_name varchar(50) NOT NULL,
+    last_name varchar(50) NOT NULL,
+    bio varchar(70),
     birthday date,
     avatar_url varchar,
 
@@ -20,8 +20,8 @@ CREATE TABLE sessions (
     id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
     user_id uuid NOT NULL,
     refresh_token varchar NOT NULL,
-    user_agent varchar NOT NULL, -- To show "Logged in on Chrome/Windows"
-    client_ip varchar NOT NULL,  -- For security alerts
+    user_agent varchar(255) NOT NULL, -- To show "Logged in on Chrome/Windows"
+    client_ip varchar(45) NOT NULL,  -- For security alerts
     is_blocked boolean NOT NULL DEFAULT false, -- To revoke a specific device
     expires_at timestamptz NOT NULL,
     created_at timestamptz NOT NULL DEFAULT (now())
