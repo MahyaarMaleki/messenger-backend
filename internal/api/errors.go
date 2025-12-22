b0361fef1a4be4faf8322d7b0fd5cea7165e7b23
Mahyar Maleki <mahyarmaleki2003@gmail.com>
2025-12-22 18:37:32 +0330

    Define custom error responses for validation

--- internal/api/errors.go
+++ internal/api/errors.go
@@ -2,7 +2,9 @@ package api
 
 import (
 	"errors"
+	"fmt"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/jackc/pgx/v5/pgconn"
 )
 
@@ -10,6 +12,11 @@ const (
 	ForeignKeyViolation = "23503"
 	UniqueViolation     = "23505"
 )
+
+type validationResponse struct {
+	Error   string            `json:"error"`
+	Details map[string]string `json:"details"`
+}
 
 // errorCode extracts the SQL State code
 func errorCode(err error) string {
@@ -21,9 +28,40 @@ func errorCode(err error) string {
 	return ""
 }
 
-// errorResponse wraps the error message in a JSON object
 func errorResponse(err error) map[string]any {
 	return map[string]any{
 		"error": err.Error(),
+	}
+}
+
+func validationErrorResponse(err error) validationResponse {
+	details := make(map[string]string)
+
+	var valErrors validator.ValidationErrors
+	if errors.As(err, &valErrors) {
+		for _, fe := range valErrors {
+			details[fe.Field()] = msgForTag(fe)
+		}
+	}
+
+	return validationResponse{
+		Error:   "Invalid input parameters",
+		Details: details,
+	}
+}
+
+// msgForTag customizes the error message for each validation tag
+func msgForTag(fe validator.FieldError) string {
+	switch fe.Tag() {
+	case "required":
+		return "This field is required"
+	case "email":
+		return "Invalid email format"
+	case "min":
+		return fmt.Sprintf("Must be at least %s characters long", fe.Param())
+	case "max":
+		return fmt.Sprintf("Must be at most %s characters long", fe.Param())
+	default:
+		return fe.Field()
 	}
 }
