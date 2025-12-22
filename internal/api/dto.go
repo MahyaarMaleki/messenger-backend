36b2b3d27b1833732e77c9c289e1ec517546dd55
Mahyar Maleki <mahyarmaleki2003@gmail.com>
2025-12-22 21:09:32 +0330

    Define dtos and move previous request and responses along helper functions there

---
+++ internal/api/dto.go
@@ -0,0 +1,72 @@
+package api
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/mahyaarmaleki/messenger-backend/internal/db"
+)
+
+type (
+	createUserRequest struct {
+		Username  string `json:"username" validate:"required,min=3,max=30"`
+		Password  string `json:"password" validate:"required"`
+		Email     string `json:"email" validate:"required,email"`
+		FirstName string `json:"firstName" validate:"required,max=50"`
+		LastName  string `json:"lastName" validate:"required,max=50"`
+	}
+
+	loginUserRequest struct {
+		Username string `json:"username" validate:"required"`
+		Password string `json:"password" validate:"required"`
+	}
+
+	// userResponse (Private) - For Register/Login/Me
+	userResponse struct {
+		ID        uuid.UUID `json:"id"`
+		Username  string    `json:"username"`
+		Email     string    `json:"email"`
+		FirstName string    `json:"firstName"`
+		LastName  string    `json:"lastName"`
+		CreatedAt time.Time `json:"createdAt"`
+	}
+
+	// userProfileResponse (Public) - For Search/Profile View
+	userProfileResponse struct {
+		Username  string `json:"username"`
+		FirstName string `json:"firstName"`
+		LastName  string `json:"lastName"`
+		Bio       string `json:"bio"`
+		AvatarUrl string `json:"avatarUrl"`
+	}
+
+	loginUserResponse struct {
+		SessionID             uuid.UUID    `json:"sessionId"`
+		AccessToken           string       `json:"accessToken"`
+		AccessTokenExpiresAt  time.Time    `json:"accessTokenExpiresAt"`
+		RefreshToken          string       `json:"refreshToken"`
+		RefreshTokenExpiresAt time.Time    `json:"refreshTokenExpiresAt"`
+		User                  userResponse `json:"user"`
+	}
+)
+
+func newUserResponse(user db.User) userResponse {
+	return userResponse{
+		ID:        user.ID,
+		Username:  user.Username,
+		Email:     user.Email,
+		FirstName: user.FirstName,
+		LastName:  user.LastName,
+		CreatedAt: user.CreatedAt.Time,
+	}
+}
+
+func newUserProfileResponse(user db.User) userProfileResponse {
+	return userProfileResponse{
+		Username:  user.Username,
+		FirstName: user.FirstName,
+		LastName:  user.LastName,
+		Bio:       user.Bio.String,
+		AvatarUrl: user.AvatarUrl.String,
+	}
+}
