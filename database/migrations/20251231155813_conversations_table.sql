4c6f8b49087d5341e42113c4dae15fcb13c3ba6c
Mahyar Maleki <mahyarmaleki2003@gmail.com>
2026-02-18 19:14:53 +0330

    Revert not null for name field

--- database/migrations/20251231155813_conversations_table.sql
+++ database/migrations/20251231155813_conversations_table.sql
@@ -1,8 +1,7 @@
 -- +goose Up
 CREATE TABLE conversations (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    -- For private chats the other user's avatar url will be set
-    name VARCHAR(100) NOT NULL,
+    name VARCHAR(100),
     -- type: 'private' (1-on-1), 'group' (Many-to-Many), 'channel' (One-to-Many)
     type VARCHAR(20) NOT NULL DEFAULT 'private',
     -- For private chats the other user's avatar url will be set
