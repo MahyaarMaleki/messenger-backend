9a68f374a06f90ff269145c59fa7528d49ba25e1
Mahyar Maleki <mahyarmaleki2003@gmail.com>
2026-02-18 19:01:48 +0330

    Make the name field of conversations not null

--- database/migrations/20251231155813_conversations_table.sql
+++ database/migrations/20251231155813_conversations_table.sql
@@ -1,11 +1,11 @@
 -- +goose Up
 CREATE TABLE conversations (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    -- NULL for 1-on-1 chats, Set for groups and channels
-    name VARCHAR(100),
+    -- For private chats the other user's avatar url will be set
+    name VARCHAR(100) NOT NULL,
     -- type: 'private' (1-on-1), 'group' (Many-to-Many), 'channel' (One-to-Many)
     type VARCHAR(20) NOT NULL DEFAULT 'private',
-    --For private chats the other user avatar url will be set
+    -- For private chats the other user's avatar url will be set
     avatar_url VARCHAR NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
     last_message_at TIMESTAMPTZ NOT NULL DEFAULT now()
