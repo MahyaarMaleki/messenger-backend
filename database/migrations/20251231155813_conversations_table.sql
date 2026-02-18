53063b6c5db55bfe9fa0b1d486c6e2b25c97ab25
Mahyar Maleki <mahyarmaleki2003@gmail.com>
2026-02-18 18:22:50 +0330

    Add avatar_url field

--- database/migrations/20251231155813_conversations_table.sql
+++ database/migrations/20251231155813_conversations_table.sql
@@ -1,12 +1,12 @@
 -- +goose Up
 CREATE TABLE conversations (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-
     -- NULL for 1-on-1 chats, Set for groups and channels
     name VARCHAR(100),
-
     -- type: 'private' (1-on-1), 'group' (Many-to-Many), 'channel' (One-to-Many)
     type VARCHAR(20) NOT NULL DEFAULT 'private',
+    --For private chats the other user avatar url will be set
+    avatar_url VARCHAR NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
     last_message_at TIMESTAMPTZ NOT NULL DEFAULT now()
 );
